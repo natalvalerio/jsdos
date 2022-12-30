72755d17035a542c69ff9bff00a0a78190189cbe
natalvalerio <natalvalerio@gmail.com>
2022-12-30 16:14:01 -0300

    pac_4

--- sobe.bat
+++ sobe.bat
@@ -1,3 +1,15 @@
+@echo off
+cls
+echo --------------------------------------------
+echo Adicionando todos os arquivos... 
 git add . 
-git commit -m %1
+echo .
+echo --------------------------------------------
+echo Criando o commit -M %1
+git commit -M %1
+echo .
+echo --------------------------------------------
+echo Empurrando as alterações para o GitHub
 git push -u origin main 
+echo --------------------------------------------
+echo .
