37387aa8973d6c361828e4fa04724b284cd00e7a
natalvalerio <natalvalerio@gmail.com>
2026-05-06 11:12:05 -0300

    update 06/05/2026 11:12:05

--- sobe.bat
+++ sobe.bat
@@ -5,8 +5,8 @@ echo Adicionando todos os arquivos...
 git add . 
 echo .
 echo --------------------------------------------
-echo Criando o commit -m "sobe"
-git commit -m "sobe"
+echo Criando o commit -m "update %date% %time:~0,8%"
+git commit -m "update %date% %time:~0,8%"
 rem git commit -m %1
 echo .
 echo --------------------------------------------
@@ -15,3 +15,12 @@ git push
 rem git push -u origin main 
 echo --------------------------------------------
 echo .
+
+
+rem git pull
+rem git add . 
+rem git commit -m 'sonhagro' 
+rem git commit -m "update %date% %time:~0,8%"
+rem git push --force 
+rem git push origin main
+rem powershell -c "[console]::beep(1000,1000)" 
--- vai.bat
+++
@@ -1,7 +0,0 @@
-git pull
-git add . 
-rem git commit -m 'sonhagro' 
-git commit -m "update %date% %time:~0,8%"
-rem git push --force 
-git push origin main
-rem powershell -c "[console]::beep(1000,1000)" 
